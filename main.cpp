f34c9a02ff08fbeb7be014a835aa4a838e520e86
jcnelson <jcnelson@cs.princeton.edu>
2014-08-25 01:18:44 -0400

    Add GPLv3 license text

--- main.cpp
+++ main.cpp
@@ -1,5 +1,19 @@
 /*
-   License goes here.
+   loginfs: a FUSE filesystem to allow unprivileged users to access privileged files on UNIX-like systems.
+   Copyright (C) 2014  Jude Nelson
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include "main.h"
