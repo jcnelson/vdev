0e894e82845f954aad044cb625e1a4bb8f8bb108
jcnelson <jcnelson@cs.princeton.edu>
2014-12-30 23:46:39 -0500

    Dual-license vdev under both GPLv3+ and ISC

--- acl.h
+++ acl.h
@@ -2,18 +2,21 @@
    vdev: a virtual device manager for *nix
    Copyright (C) 2014  Jude Nelson
 
-   This program is free software: you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation, either version 3 of the License, or
-   (at your option) any later version.
+   This program is dual-licensed: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License version 3 or later as 
+   published by the Free Software Foundation. For the terms of this 
+   license, see LICENSE.LGPLv3+ or <http://www.gnu.org/licenses/>.
 
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
+   You are free to use this program under the terms of the GNU General
+   Public License, but WITHOUT ANY WARRANTY; without even the implied 
+   warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+   See the GNU General Public License for more details.
 
-   You should have received a copy of the GNU General Public License
-   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+   Alternatively, you are free to use this program under the terms of the 
+   Internet Software Consortium License, but WITHOUT ANY WARRANTY; without
+   even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+   For the terms of this license, see LICENSE.ISC or 
+   <http://www.isc.org/downloads/software-support-policy/isc-license/>.
 */
 
 #ifndef _VDEV_ACL_H_
