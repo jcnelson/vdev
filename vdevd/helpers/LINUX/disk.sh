cc3ee3691de4bac9253b2e84f7828f146b2f075f
jcnelson <jcnelson@cs.princeton.edu>
2015-01-25 22:56:12 -0500

    Add support for /dev/disk/by-uuid

--- vdevd/helpers/LINUX/disk.sh
+++ vdevd/helpers/LINUX/disk.sh
@@ -17,7 +17,7 @@ DISK_NAME="ata-$DISK_ID"
 # see if this is a partition...
 if [ $VDEV_OS_DEVTYPE == "partition" ]; then 
 
-   PART=$(cat /sys/dev/block/$VDEV_MAJOR\:$VDEV_MINOR/partition)
+   PART=$(/bin/cat /sys/dev/block/$VDEV_MAJOR\:$VDEV_MINOR/partition)
 
    test -z $PART && exit 0
 
@@ -25,7 +25,17 @@ if [ $VDEV_OS_DEVTYPE == "partition" ]; then
    DISK_NAME=$DISK_NAME-part${PART}
 fi
 
+# install by-id
 /bin/mkdir -p $VDEV_MOUNTPOINT/disk/by-id
 /bin/ln -s ../../$VDEV_PATH $VDEV_MOUNTPOINT/disk/by-id/$DISK_NAME
+
+# get UUID
+UUID=$(/sbin/blkid | /bin/grep $VDEV_PATH | /bin/sed 's/[^ ]* UUID="\([^ ]*\)" .*/\1/g')
+
+test -z "$UUID" && exit 0
+
+# install by-uuid
+/bin/mkdir -p $VDEV_MOUNTPOINT/disk/by-uuid
+/bin/ln -s ../../$VDEV_PATH $VDEV_MOUNTPOINT/disk/by-uuid/$UUID
 
 exit 0
