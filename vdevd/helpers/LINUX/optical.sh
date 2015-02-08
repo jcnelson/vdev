2b04d1ea5dad850fee72ef2a08fd831db0034a5d
jcnelson <jcnelson@cs.princeton.edu>
2015-02-08 00:46:01 -0500

    Support adding, removing, and testing device links in optical.sh (needs testing)

--- vdevd/helpers/LINUX/optical.sh
+++ vdevd/helpers/LINUX/optical.sh
@@ -6,12 +6,32 @@ STAT_OPTICAL=$VDEV_HELPERS/stat_optical
 
 # set up capability environment variables 
 eval $($STAT_OPTICAL $VDEV_MOUNTPOINT/$VDEV_PATH)
+STAT_OPTICAL_EXIT=$?
+
+# verify that we stat'ed the optical device...
+test 0 -ne $STAT_OPTICAL_EXIT && exit 0
+
+test_device_symlink() {
+
+   TARGET=$1
+
+   echo "ln -s $VDEV_PATH $VDEV_MOUNTPOINT/$TARGET"
+   return $?
+}
 
 set_device_symlink() {
    
    TARGET=$1
 
-   /bin/ln -s $VDEV_PATH $VDEV_MOUNTPOINT/$TARGET 2>/dev/null
+   /bin/ln -s $VDEV_PATH $VDEV_MOUNTPOINT/$TARGET
+   return $?
+}
+
+remove_device_symlink() {
+   
+   TARGET=$1
+
+   /bin/rm -f $VDEV_MOUNTPOINT/$TARGET
    return $?
 }
 
@@ -30,6 +50,7 @@ add_device_symlink() {
    return 0
 }
 
+
 # always add 'cdrom'
 set_device_symlink "cdrom"
 
@@ -38,3 +59,46 @@ add_device_symlink $VDEV_PATH $VDEV_OPTICAL_CD_RW "cdrw"
 add_device_symlink $VDEV_PATH $VDEV_OPTICAL_DVD "dvd"
 add_device_symlink $VDEV_PATH $VDEV_OPTICAL_DVD_R "dvdrw"
 add_device_symlink $VDEV_PATH $VDEV_OPTICAL_DVD_RAM "dvdrw"
+
+case "$VDEV_ACTION" in
+
+   add)
+   
+      set_device_symlink "cdrom"
+      
+      add_device_symlink $VDEV_PATH $VDEV_OPTICAL_CD_R "cdrw"
+      add_device_symlink $VDEV_PATH $VDEV_OPTICAL_CD_RW "cdrw"
+      add_device_symlink $VDEV_PATH $VDEV_OPTICAL_DVD "dvd"
+      add_device_symlink $VDEV_PATH $VDEV_OPTICAL_DVD_R "dvdrw"
+      add_device_symlink $VDEV_PATH $VDEV_OPTICAL_DVD_RAM "dvdrw"
+      
+      ;;
+
+   remove)
+
+      remove_device_symlink "cdrom"
+      
+      remove_device_symlink $VDEV_PATH $VDEV_OPTICAL_CD_R "cdrw"
+      remove_device_symlink $VDEV_PATH $VDEV_OPTICAL_CD_RW "cdrw"
+      remove_device_symlink $VDEV_PATH $VDEV_OPTICAL_DVD "dvd"
+      remove_device_symlink $VDEV_PATH $VDEV_OPTICAL_DVD_R "dvdrw"
+      remove_device_symlink $VDEV_PATH $VDEV_OPTICAL_DVD_RAM "dvdrw"
+
+      ;;
+
+   test)
+
+      
+      test_device_symlink "cdrom"
+      
+      test_device_symlink $VDEV_PATH $VDEV_OPTICAL_CD_R "cdrw"
+      test_device_symlink $VDEV_PATH $VDEV_OPTICAL_CD_RW "cdrw"
+      test_device_symlink $VDEV_PATH $VDEV_OPTICAL_DVD "dvd"
+      test_device_symlink $VDEV_PATH $VDEV_OPTICAL_DVD_R "dvdrw"
+      test_device_symlink $VDEV_PATH $VDEV_OPTICAL_DVD_RAM "dvdrw"
+
+      ;;
+
+esac
+
+exit 0
