1674a21c8184fc96f58558279ef73ede8361846c
jcnelson <jcnelson@cs.princeton.edu>
2015-02-22 18:49:02 -0500

    usb-name.sh should only concern itself with catching "UNKNOWN" devices and setting up /dev/bus (for now)

--- vdevd/helpers/LINUX/usb-name.sh
+++ vdevd/helpers/LINUX/usb-name.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
 
+# only deal with devices we know exist
+if [ "$VDEV_PATH" == "UNKNOWN" ]; then 
+   exit 0
+fi
+
 # if we have a bus and device number, use them
 if [ -n "$VDEV_OS_BUSNUM" -a -n "$VDEV_OS_DEVNUM" ]; then 
 
    # USB bus device
    echo -n "bus/usb/$VDEV_OS_BUSNUM/$VDEV_OS_DEVNUM"
-
-else
-
-   # some other USB device
-   # TODO: need other handlers
-   echo -n $VDEV_PATH
 fi
 
 
