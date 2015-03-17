b6157a80261fe49faf814d47298f33a944a640d4
Jude Nelson <judecn@devuan.org>
2015-03-17 00:49:48 -0400

    Add a firmware loader helper, and an action to load firmware for a device automatically (after modprobe, but before any further actions are taken).

---
+++ vdevd/helpers/LINUX/firmware.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+. $VDEV_HELPERS/subr.sh
+
+# if instructed to load firmware, do so
+if [ -n "$VDEV_OS_FIRMWARE" -a -n "$VDEV_OS_DEVPATH" ]; then 
+   
+   load_firmware $VDEV_OS_DEVPATH $VDEV_FIRMARE_DIR/$VDEV_OS_FIRMWARE 
+   
+   _RC=$?
+
+   if [ $_RC -ne 0 ]; then 
+      vdev_error "Failed to load formware '$VDEV_FIRMARE_DIR/$VDEV_OS_FIRMWARE' for '$VDEV_OS_DEVPATH'"
+   fi
+   
+fi
+
+exit 0
