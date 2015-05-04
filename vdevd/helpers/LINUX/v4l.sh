9b456b1c0e5b465d7f5c5fcfcbe9347065f05c2f
Jude Nelson <judecn@gmail.com>
2015-05-03 23:20:58 -0400

    Add /dev/v4l/by-path symlinks

--- vdevd/helpers/LINUX/v4l.sh
+++ vdevd/helpers/LINUX/v4l.sh
@@ -30,7 +30,15 @@ PATH=
 TYPE_PLUS_INDEX=$(echo $VDEV_PATH | /bin/sed -r 's/.*\/([^/]+)/\1/g')
 
 TYPE=$(echo $TYPE_PLUS_INDEX | /bin/sed -r 's/[0-9]+$//g')
-INDEX=$(/bin/cat $VDEV_OS_SYSFS_MOUNTPOINT/$VDEV_OS_DEVPATH/index)
+INDEX=
+
+if [ -f $VDEV_OS_SYSFS_MOUNTPOINT/$VDEV_OS_DEVPATH/index ]; then 
+   INDEX=$(/bin/cat $VDEV_OS_SYSFS_MOUNTPOINT/$VDEV_OS_DEVPATH/index)
+else 
+
+   # no index? no persistent paths
+   exit 0
+fi
 
 # is this a USB device?
 if [ -n "$(echo $VDEV_OS_DEVPATH | /bin/grep 'usb')" ]; then 
@@ -52,7 +60,27 @@ if [ -n "$BUS" -a -n "$INDEX" -a -n "$VENDOR" -a -n "$MODEL" -a -n "$TYPE" ]; th
    vdev_symlink ../../$VDEV_PATH $VDEV_MOUNTPOINT/v4l/by-id/$ID $VDEV_METADATA
 fi
 
-# TODO: by-path
+# by-path 
+VDEV_PERSISTENT_PATH=
+eval $($VDEV_HELPERS/stat_path $VDEV_MOUNTPOINT/$VDEV_PATH)
+RC=$?
+
+[ $RC -ne 0 ] && vdev_fail 1 "$VDEV_HELPERS/stat_path $VDEV_MOUNTPOINT/$VDEV_PATH exit status $RC"
+
+# did we get a path?
+[ -z "$VDEV_PERSISTENT_PATH" ] && exit 0
+
+if [ -n "$(echo $VDEV_PATH | /bin/egrep "video|vbi")" ]; then 
+   
+   # video v4l device 
+   vdev_symlink ../../$VDEV_PATH $VDEV_MOUNTPOINT/v4l/by-path/$VDEV_PERSISTENT_PATH-video-index${INDEX} $VDEV_METADATA
+fi
+
+elif [ -n "$(echo $VDEV_PATH | /bin/egrep "audio")" ]; then 
+
+   # audio v4l device 
+   vdev_symlink ../../$VDEV_PATH $VDEV_MOUNTPOINT/v4l/by-path/$VDEV_PERSISTENT_PATH-audio-index${INDEX} $VDEV_METADATA
+fi
 
 # set up permissions...
 vdev_permissions root.video 0660 $VDEV_MOUNTPOINT/$VDEV_PATH
