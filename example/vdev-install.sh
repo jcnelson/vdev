e9302c78146d42c629e4f8878d98a4c66d7fc501
root <root@devuan.devuan.org>
2015-05-11 13:58:01 +0000

    vdev init script installer

---
+++ example/vdev-install.sh
@@ -0,0 +1,20 @@
+#!/bin/sh -e
+
+PREFIX=/
+
+if [ $# -gt 1 ]; then 
+   PREFIX=$1
+   shift 1
+fi
+
+# make sure we installed vdev 
+test -x $PREFIX/etc/init.d/vdev || (echo "You must install vdev first" && exit 1)
+
+test -d $PREFIX/etc/vdev || (echo "You must install vdev first" && exit 1)
+
+update-rc.d -f udev remove
+update-rc.d -f udev-finish remove
+update-rc.d -f vdev defaults
+insserv
+
+exit 0
