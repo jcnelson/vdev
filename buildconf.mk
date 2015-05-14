a5185e5a022b66f6ee144662be22f44cc15e89ed
Jude Nelson <judecn@gmail.com>
2015-05-13 22:11:50 -0400

    Revamped Makefiles to use them "correctly": * output files are written to a separate build/ directory. * every file to be built, installed, and uninstalled is a target. * no side-effects for non-PHONY targets. * global build options and variables set in buildconf.mk * remove unnecessary Makefiles.

---
+++ buildconf.mk
@@ -0,0 +1,75 @@
+# build environment
+ROOT_DIR := $(shell dirname $(realpath $(lastword $(MAKEFILE_LIST))))
+BUILD    ?= $(ROOT_DIR)/build
+OS ?= LINUX
+BUILD_BINDIR := $(BUILD)/bin
+BUILD_SBINDIR := $(BUILD)/sbin
+BUILD_LIBDIR := $(BUILD)/lib
+BUILD_USRBIN := $(BUILD)/usr/bin
+BUILD_USRSHARE := $(BUILD)/usr/share
+BUILD_USRSBIN := $(BUILD)/usr/sbin
+BUILD_INCLUDEDIR := $(BUILD)/include/
+BUILD_ETCDIR := $(BUILD)/etc
+
+# install environment
+PREFIX         ?= /usr/local
+BINDIR         ?= $(PREFIX)/bin
+SBINDIR			?= $(PREFIX)/sbin
+LIBDIR         ?= $(PREFIX)/lib
+INCLUDEDIR     ?= /usr/local/include
+USRBINDIR		?= /usr/bin
+USRSBINDIR		?= /usr/sbin
+USRSHAREDIR		?= /usr/share
+PKGCONFIGDIR   ?= $(PREFIX)/lib/pkgconfig
+ETCDIR			?= /etc
+
+# libvdev 
+BUILD_LIBVDEV := $(BUILD_LIBDIR)
+BUILD_LIBVDEV_HEADERS := $(BUILD_INCLUDEDIR)/libvdev
+BUILD_LIBVDEV_DIRS := $(BUILD_LIBVDEV) $(BUILD_LIBVDEV_HEADERS)
+INSTALL_LIBVDEV := $(LIBDIR)
+
+# vdevd 
+BUILD_VDEVD := $(BUILD_SBINDIR)
+BUILD_VDEVD_HELPERS := $(BUILD_LIBDIR)/vdev
+BUILD_VDEVD_DIRS := $(BUILD_VDEVD) $(BUILD_VDEVD_HELPERS)
+INSTALL_VDEVD := $(SBINDIR)
+INSTALL_VDEVD_HELPERS := $(LIBDIR)/vdev
+
+# vdevfs 
+BUILD_VDEVFS := $(BUILD_USRSBIN)
+BUILD_VDEVFS_DIRS := $(BUILD_VDEVFS)
+INSTALL_VDEVFS := $(USRSBINDIR)
+
+# config
+BUILD_VDEV_CONFIG := $(BUILD_ETCDIR)/vdev
+BUILD_VDEV_INITSCRIPT := $(BUILD_ETCDIR)/init.d
+BUILD_VDEV_INITRAMFS := $(BUILD_USRSHARE)/initramfs-tools
+BUILD_VDEV_CONFIG_DIRS := $(BUILD_VDEV_CONFIG)
+INSTALL_VDEV_CONFIG := $(ETCDIR)/vdev
+INSTALL_VDEV_INITSCRIPT := $(ETCDIR)/init.d
+INSTALL_VDEV_INITRAMFS := $(USRSHAREDIR)/initramfs-tools
+
+# compiler
+CFLAGS     := -Wall -std=c99 -g -fPIC -fstack-protector -fstack-protector-all -pthread -Wno-unused-variable -Wno-unused-but-set-variable
+CPPFLAGS   := -Wall -g -fPIC -fstack-protector -fstack-protector-all -pthread -Wno-unused-variable -Wno-unused-but-set-variable
+INC      := -I. -I$(ROOT_DIR) -I$(BUILD_INCLUDEDIR)
+DEFS     := -D_THREAD_SAFE -D__STDC_FORMAT_MACROS -D_VDEV_OS_$(OS)
+LIBINC   := -L$(BUILD_LIBVDEV)
+CC       := gcc $(CFLAGS) $(INC) $(DEFS)
+CPP      := g++ $(CPPFLAGS) $(INC) $(DEFS)
+
+# build setup
+BUILD_DIRS   := $(BUILD_VDEVD_DIRS) \
+                $(BUILD_VDEVFS_DIRS) \
+					 $(BUILD_LIBVDEV_DIRS)
+
+all:
+
+build_setup: $(BUILD_DIRS)
+
+$(BUILD_DIRS):
+	@mkdir -p $@
+
+# debugging...
+print-%: ; @echo $*=$($*)
