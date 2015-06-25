1c59f962473aa6344bd055f102936dab8a245690
Jude Nelson <judecn@gmail.com>
2015-06-25 00:23:52 -0400

    Query the config file directly to find the firmware directory

--- vdevd/helpers/LINUX/firmware.sh
+++ vdevd/helpers/LINUX/firmware.sh
@@ -2,10 +2,53 @@
 
 . "$VDEV_HELPERS/subr.sh"
 
+
+
+# load firmware for a device 
+# $1   the sysfs device path 
+# $2   the path to the firmware 
+# return 0 on success
+# return 1 on error
+firmware_load() {
+   
+   local _SYSFS_PATH _FIRMWARE_PATH _SYSFS_FIRMWARE_PATH _RC
+   
+   _SYSFS_PATH="$1"
+   _FIRMWARE_PATH="$2"
+   _SYSFS_FIRMWARE_PATH="$VDEV_OS_SYSFS_MOUNTPOINT/$_SYSFS_PATH"
+
+   test -e "$_SYSFS_FIRMWARE_PATH/loading" || return 1
+   test -e "$_SYSFS_FIRMWARE_PATH/data" || return 1
+   
+   echo 1 > "$_SYSFS_FIRMWARE_PATH/loading"
+   /bin/cat "$_FIRMWARE_PATH" > "$_SYSFS_FIRMWARE_PATH/data"
+   
+   _RC=$?
+   if [ $_RC -ne 0 ]; then 
+      # abort 
+      echo -1 > "$_SYSFS_FIRMWARE_PATH/loading"
+   else 
+      # success
+      echo 0 > "$_SYSFS_FIRMWARE_PATH/loading"
+   fi
+
+   return $_RC
+}
+
+
 # if instructed to load firmware, do so
 if [ -n "$VDEV_OS_FIRMWARE" -a -n "$VDEV_OS_DEVPATH" ]; then 
    
-   vdev_firmware_load "$VDEV_OS_DEVPATH" "$VDEV_FIRMARE_DIR/$VDEV_OS_FIRMWARE"
+   # load firmware dir 
+   eval "$(vdev_parse_config "$VDEV_CONFIG_FILE" "VDEV_" | /bin/grep "VDEV_firmware")"
+   VDEV_FIRMARE_DIR="$VDEV_firmware"
+
+   if [ -z "$VDEV_FIRMARE_DIR" ] || ! [ -d "$VDEV_FIRMARE_DIR" ]; then 
+
+      vdev_fail 1 "Firmware directory not found at $VDEV_FIRMARE_DIR"
+   fi
+
+   firmware_load "$VDEV_OS_DEVPATH" "$VDEV_FIRMARE_DIR/$VDEV_OS_FIRMWARE"
    
    _RC=$?
 
