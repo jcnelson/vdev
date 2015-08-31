7360181406d6981ef5ee74892cd5f56300515e5f
Jude Nelson <judecn@gmail.com>
2015-08-31 00:53:25 -0400

    Build the initramfs by merging the user's /etc/initramfs and /usr/share/initramfs (minus udev files) into build/, and then merging vdev's initramfs scripts on top of them.  This is super-hacky and will not be used in production, but it at least gives a way to make an initramfs without having to *uninstall* udev first.

--- example/make-initramfs.sh
+++ example/make-initramfs.sh
@@ -1,14 +1,13 @@
 #!/bin/sh -e
 
-BUILD_INITRAMFS=build/initramfs
+BUILD_INITRAMFS=../build/initramfs
+BUILD_INITRAMFS_CONFDIR=../build/etc/initramfs-tools
 VDEV_INITRAMFS_FILES=initramfs
 MKINITRAMFS=../tools/initramfs/mkinitramfs
 
-trap "rm -rf \"$BUILD_INITRAMFS\"" 0 1 2 3 15
+trap "rm -rf \"$BUILD_INITRAMFS\" \"$BUILD_INITRAMFS_CONFDIR\"" 0 1 2 3 15
 
-mkdir -p $BUILD_INITRAMFS
-mkdir -p $BUILD_INITRAMFS/vdev
-mkdir -p $BUILD_INITRAMFS/init.d
+mkdir -p "$BUILD_INITRAMFS"
 
 test -d /usr/share/initramfs-tools || (echo "You must install the initramfs-tools package" && exit 1)
 test -e /sbin/vdevd || (echo "You must install vdevd first" && exit 1)
@@ -17,28 +16,51 @@ test -e /etc/vdev || (echo "You must install vdev's /etc files first" && exit 1)
 # copy over initramfs base files, but skip the udev ones
 for initramfs_file in $(find /usr/share/initramfs-tools); do
 
-   if [ -d $initramfs_file ]; then 
+   if [ -d "$initramfs_file" ]; then 
       continue
    fi
 
-   if [ -n "$(echo $initramfs_file | grep "udev")" ]; then 
+   if [ -n "$(echo "$initramfs_file" | grep "udev")" ]; then 
       # skip udev file 
       continue
    fi
 
    base_file=${initramfs_file##/usr/share/initramfs-tools/}
    
-   mkdir -p $BUILD_INITRAMFS/$(dirname $base_file)
-   cp -a $initramfs_file $BUILD_INITRAMFS/$base_file
+   mkdir -p "$BUILD_INITRAMFS/$(dirname "$base_file")"
+   cp -a "$initramfs_file" "$BUILD_INITRAMFS/$base_file"
 
 done
 
-# copy over vdev's initramfs files 
-cp -a $VDEV_INITRAMFS_FILES/* $BUILD_INITRAMFS/
+# copy over initramfs conf files, but skip the udev ones
+for initramfs_file in $(find /etc/initramfs-tools); do
+
+   if [ -d "$initramfs_file" ]; then 
+      continue
+   fi
+
+   if [ -n "$(echo "$initramfs_file" | grep "udev")" ]; then 
+      # skip udev file 
+      continue
+   fi
+
+   base_file=${initramfs_file##/etc/initramfs-tools/}
+   
+   mkdir -p "$BUILD_INITRAMFS_CONFDIR/$(dirname "$base_file")"
+   cp -a "$initramfs_file" "$BUILD_INITRAMFS_CONFDIR/$base_file"
+
+done
+
+# copy over vdev's init and modules... 
+cp -a "$VDEV_INITRAMFS_FILES/init" "$BUILD_INITRAMFS"
+
+# copy over vdev's initramfs files to conf 
+cp -a "$VDEV_INITRAMFS_FILES"/* "$BUILD_INITRAMFS_CONFDIR"/
+rm "$BUILD_INITRAMFS_CONFDIR"/init
 
 # build it!
 echo "Generating initrd.img-`uname -r`"
-$MKINITRAMFS -t $BUILD_INITRAMFS -o initrd.img-`uname -r`
+"$MKINITRAMFS" -t "$BUILD_INITRAMFS" -d "$BUILD_INITRAMFS_CONFDIR" -o initrd.img-`uname -r`
 
 if [ $? -eq 0 ]; then 
    echo "Generated initrd.img-`uname -r`"
